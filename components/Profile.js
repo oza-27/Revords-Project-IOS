2b5752c8dab4b3197c9ce0d872c458b4d41cd453
oza-27 <ozasoumy2000@gmail.com>
2024-03-12 12:57:30 +0530

    Check

--- components/Profile.js
+++ components/Profile.js
@@ -8,7 +8,6 @@ import { Link, useIsFocused } from "@react-navigation/native";
 import Globals from './Globals';
 import ImageViewer from 'react-native-image-zoom-viewer';
 
-
 const Profile = ({ route, navigation }) => {
     const focus = useIsFocused();
     const [name, setName] = useState('');
